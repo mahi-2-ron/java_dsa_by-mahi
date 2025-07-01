f2cf31761ec70fcb77ff1d1c8a4af9ef9dd3de5e
Mahesh <maheshrmadiwalar@gmail.com>
2025-07-01 11:19:44 +0530

    First commit

--- serching.java
+++ serching.java
@@ -1,28 +1,71 @@
 public class serching {
-  
-  
-   
 
-   public static int linearSearch(int[] var0, int var1) {
-      int var2 = var0.length;
+   public static int linearSearch(int arr[], int key) {
+      int n = arr.length;
 
-      for(int var3 = 0; var3 < var2; ++var3) {
-         if (var0[var3] == var1) {
-            return var3;
+      for(int i = 0; i < n; ++i) {
+         if (arr[i] == key) {
+            return i;
          }
       }
 
       return -1;
    }
+   public static int binarySearch(int arr[],int key){
+    int n = arr.length;
+    int low = 0;
+    int high = n-1;
+    
+    while(high >= low){
+     int mid = low + (high - low ) / 2;
+        if(arr[mid] == key){
+     
+            return mid;
+            
+        }
+        else if(key >= arr[mid]){
+            low = mid + 1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return -1;
 
-   public static void main(String[] var0) {
-      int[] var1 = new int[]{2, 4, 6, 8};
-      int var2 = linearSearch(var1, 2);
-      if (var2 >= 0) {
-         System.out.println("The key found at the index of " + var2);
-      } else {
-         System.out.println("The key isn't found");
-      }
+   }
+   public static void insertonSort(int arr[]){
+    int n = arr.length;
+    for(int i=1;i<n;i++){
+        int j = i - 1;
+        int key = arr[i];
+        while( j>=0 && arr[j] >= key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key ;
+    }
+   }
+
+   public static void main(String[] args) {
+   
+
+    int arr [] = {1,2,3,4,5,6,7,8,9};
+    int res = binarySearch(arr, 7);
+    if(res >= 0){
+        System.out.println("The key found at the index :"+res);
+    }else{
+        System.out.println("The key is not founding");
+    }
+    System.out.println(res);
+    System.out.println("Om ganeshaya namaha");
+    int num[] = { 0,5,8,4,6,2};
+    insertonSort(num);
+    //PRINTING THE ARRAY WHICH WILL BE SORT
+    for(int i = 0;i<num.length;i++){
+        System.out.println(num[i]);
+    }
+
+
+
 
    }
 }
